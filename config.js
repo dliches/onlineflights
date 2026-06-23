1fc7b4d29f34ecf12a3ab61d1dff46c226094bb2
dliches <dliches@hotmail.com>
2026-06-23 13:56:43 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 // This is a browser app, so only use a public/anon key here.
 // Never paste a service_role or secret key into this file.
 window.FLIGHT_TRACKER_CONFIG = {
-  SUPABASE_REST_URL: "https://vryfmkigbkeaziovjyte.supabase.co/rest/v1/",
+  SUPABASE_REST_URL: "https://vryfmkigbkeaziovjyte.supabase.co",
   SUPABASE_API_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyeWZta2lnYmtlYXppb3ZqeXRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyMDc2MzIsImV4cCI6MjA5Nzc4MzYzMn0.Y0UL7a1HSxsnVXGgnn3kJia2DIuO2muXtucToGbD6pU"
 };
